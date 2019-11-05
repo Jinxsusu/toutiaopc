b06f939d671fae18c557184b321c66c4cc1b9872
Jinx <Jinx@qq.com>
2019-11-06 00:59:31 +0800

    登录页面静态页面实现

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,57 @@
 <template>
-  <div>我是登录页面</div>
+  <div class="login">
+    <el-card class="login-card">
+      <div class="logo">
+        <img src="../../assets/img/logo_index.png" alt />
+      </div>
+      <el-form class="from">
+        <el-form-item>
+          <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item style="margin-bottom:12px">
+          <el-input style="width:200px" placeholder="验证码"></el-input>
+          <el-button  style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item style="margin-bottom:12px">
+            <el-checkbox >我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+            <el-button
+            style="width:348px"
+            type="primary">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
+<style scoped lang='less'>
+.login {
+  background-image: url("../../assets/img/login_bg.jpg");
+  height: 100vh;
+  width: 100%;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-card {
+    width: 410px;
+    height: 345px;
+    box-sizing: border-box;
+    padding: 10px;
+    .logo {
+      text-align: center;
+      img {
+        width: 165px;
+      }
+    }
+    .from {
+      margin-top: 20px;
+    }
+  }
+}
 </style>
