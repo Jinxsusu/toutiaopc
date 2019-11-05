75ac41639a921087ca930fa216fe3d7b6822ab05
Jinx <Jinx@qq.com>
2019-11-05 23:57:16 +0800

    新建登录和首页组件并挂载路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '@/views/login'
+import Home from '@/views/home'
 
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    name: 'login',
+    path: '/login',
+    component: Login
+  },
+  {
+    name: 'home',
+    path: '/',
+    component: Home
+  }
 ]
 
 const router = new VueRouter({
