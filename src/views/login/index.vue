40428fdfd67ab283ee50a447e63acb12eef5076e
Jinx <Jinx@qq.com>
2019-11-06 15:08:46 +0800

    登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,21 +4,20 @@
       <div class="logo">
         <img src="../../assets/img/logo_index.png" alt />
       </div>
-      <el-form class="from">
+      <!--登录表单  绑定整个表单-->
+      <el-form ref="loginRules" :model="fromData" :rules="loginRules" class="from">
+        <el-form-item prop="mobile">
+          <el-input placeholder="请输入手机号" v-model="fromData.mobile"></el-input>
+        </el-form-item>
+        <el-form-item style="margin-bottom:12px" prop="code">
+          <el-input v-model="fromData.code" style="width:200px" placeholder="验证码"></el-input>
+          <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item prop="check">
+          <el-checkbox v-model="fromData.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
         <el-form-item>
-          <el-input placeholder="请输入手机号"></el-input>
-        </el-form-item>
-        <el-form-item style="margin-bottom:12px">
-          <el-input style="width:200px" placeholder="验证码"></el-input>
-          <el-button  style="float:right">发送验证码</el-button>
-        </el-form-item>
-        <el-form-item style="margin-bottom:12px">
-            <el-checkbox >我已阅读并同意用户协议和隐私条款</el-checkbox>
-        </el-form-item>
-        <el-form-item>
-            <el-button
-            style="width:348px"
-            type="primary">登录</el-button>
+          <el-button @click="login" style="width:100%" type="primary">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -26,7 +25,55 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    // 自定义校验规则 是一个函数
+    let validator = (rule, value, callback) => {
+      if (value) {
+        callback() // 如果value为true直接通过
+      } else {
+        callback(new Error('您必须无条件的同意'))
+      }
+    }
+    return {
+      fromData: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      // 校验规则
+      loginRules: {
+        mobile: [
+          {
+            pattern: /^1[3456789]\d{9}$/,
+            message: '手机号格式不正确'
+            // 正则表达式 校验手机号的格式
+          }
+        ],
+        code: [
+          {
+            required: true,
+            message: '请输入验证码'
+          },
+          {
+            pattern: /^\d{6}$/,
+            message: '请输入6位有效数字'
+          }
+        ],
+        check: [
+          {
+            validator // 使用自定义校验规则
+          }
+        ]
+      }
+    }
+  },
+  method: {
+    login () {
+
+    }
+  }
+}
 </script>
 
 <style scoped lang='less'>
