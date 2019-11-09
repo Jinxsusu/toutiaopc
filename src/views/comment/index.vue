e253b9ba72eb68c8687eb77f1197836e048992d8
Jinx <Jinx@qq.com>
2019-11-09 21:50:22 +0800

    -评论列表-页面结构-table的使用

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>我是评论列表</div>
+  <el-card>
+    <!-- 插槽内容 => 标题 -->
+    <bread-crumbs slot="header">
+      <!-- 面包屑的具名插槽 -->
+      <template slot="title">评论列表</template>
+    </bread-crumbs>
+    <el-table :data="list">
+      <el-table-column prop="name" label="日期" width="700"></el-table-column>
+      <el-table-column prop="name" label="评论状态"></el-table-column>
+      <el-table-column prop="name" label="总评论数"></el-table-column>
+      <el-table-column prop="name" label="粉丝评论数"></el-table-column>
+      <el-table-column prop="name" label="操作"></el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
