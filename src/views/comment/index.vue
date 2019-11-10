694e898e390ac7405b925bed2dda80e67ce93961
Jinx <Jinx@qq.com>
2019-11-10 11:55:34 +0800

    --long                show status in long format (default)

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -6,19 +6,19 @@
       <template slot="title">评论列表</template>
     </bread-crumbs>
     <el-table :data="list">
-      <el-table-column prop="title" label="标题" width="700"></el-table-column>
-      <el-table-column :formatter="formatter" prop="comment_status" label="评论状态"></el-table-column>
-      <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
-      <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
-      <el-table-column label="操作">
+      <el-table-column prop="title" label="标题" width="600"></el-table-column>
+      <el-table-column :formatter="formatter" align="center" prop="comment_status" label="评论状态"></el-table-column>
+      <el-table-column prop="total_comment_count" align="center" label="总评论数"></el-table-column>
+      <el-table-column prop="fans_comment_count" align="center" label="粉丝评论数"></el-table-column>
+      <el-table-column label="操作" align="center">
         <template slot-scope="obj">
+          <el-button type="text" size="small">修改</el-button>
           <el-button
-           type="text"
-           size="small"
-           :style='{color:obj.row.comment_status ? "#E6A23C":"#409EFF"}'
-          >
-              {{obj. row.comment_status ? '关闭评论' : '打开评论'}}
-        </el-button>
+            type="text"
+            size="small"
+            :style="{color:obj.row.comment_status ? '#E6A23C':'#409EFF'}"
+            @click="openOrClose(obj.row)"
+          >{{obj. row.comment_status ? '关闭评论' : '打开评论'}}</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -33,6 +33,23 @@ export default {
     }
   },
   methods: {
+    // 打开或关闭评论
+    openOrClose (row) {
+      let message = row.comment_status ? '关闭' : '打开'
+      this.$confirm(`您是否要${message}评论?`, '提示').then(() => {
+        this.$axios({
+          method: 'PUT',
+          url: '/mp/v1_0/comments/status',
+          params: { article_id: row.id },
+          // 传递article_id 参数
+          data: { allow_comment: !row.comment_status }
+        // 取反 更改评论的状态
+        }).then(res => {
+          // 重新调用 获取评论的数据 重新调用,拉取数据
+          this.getComments()
+        })
+      })
+    },
     // 设置表格格式
     formatter (row) {
       return row.comment_status ? '正常' : '关闭'
